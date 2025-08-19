3c055ef8822ca2c46069f7df8214649e92dadbab
gongdori1 <gngsbgim4@gmail.com>
2025-08-19 11:36:02 +0900

    모험가 길드(어렵네...)

---
+++ Greedy_4.py
@@ -0,0 +1,16 @@
+n = int(input())
+explorers = list(map(int, input().split()))
+explorers.sort()  #1 2 2 2 3
+
+result = 0 # 총 그룹의 수 
+count = 0 # 그룹에 포함된 모험가의 수 
+
+for i in explorers:
+    count += 1
+    if count >= i:
+        result += 1
+        count = 0 # 현재 그룹에 포함된 모험가의 수 초기화
+
+print(result)
+
+
